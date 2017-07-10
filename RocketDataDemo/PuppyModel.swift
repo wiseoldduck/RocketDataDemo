1a10f0695c9c498f8cee1ac7ebe076126801b910
Kevin Smith <ksmith@csod.com>
2017-07-10 00:01:02 -0700

    Step 4 - Cache data w/RocketData

--- RocketDataDemo/PuppyModel.swift
+++ RocketDataDemo/PuppyModel.swift
@@ -7,39 +7,63 @@
 //
 
 import Foundation
+import RocketData
 
 struct PuppyService: Decodable {
     let posts: [PuppyModel]
 }
 
-struct PuppyModel:Codable {
+struct PuppyModel: Codable, Equatable {
+    var id: String
     var caption: String?
     var imageURL: String?
     
     private enum CodingKeys: String, CodingKey {
-        
+        case id
         case caption = "photo-caption"
         case imageURL = "photo-url-1280"
     }
 }
 
-extension PuppyModel {
+extension PuppyModel: SampleAppModel {
     
     init?(data: [AnyHashable: Any]) {
-        guard let caption = data["caption"] as? String,
+        guard let id = data["id"] as? String,
+            let caption = data["caption"] as? String,
             let imageURL = data["imageURL"] as? String else {
                 return nil
         }
-        
+        self.id = id
         self.caption = caption
         self.imageURL = imageURL
     }
     
     func data() -> [AnyHashable: Any] {
         return [
-            
+            "id": id,
             "caption": caption ?? "",
             "imageURL": imageURL ?? ""
         ]
     }
+}
+    
+extension PuppyModel: Model {
+    var modelIdentifier: String? {
+        // ensure this is globally unique
+        return "Puppy:\(id)"
+    }
+    
+    func map(_ transform: (Model) -> Model?) -> PuppyModel? {
+        // No child objects, so we can just return self
+        return self
+    }
+    
+    func forEach(_ visit: (Model) -> Void) {
+    }
+}
+
+func ==(lhs: PuppyModel, rhs: PuppyModel) -> Bool {
+    return lhs.id == rhs.id &&
+        lhs.caption == rhs.caption &&
+        lhs.imageURL == rhs.imageURL
 }
--- RocketDataDemo/ViewController.swift
+++ RocketDataDemo/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import RocketData
 
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
-    var collection:[PuppyModel] = []
+    /// The data provider which backs this view controller
+    fileprivate let dataProvider = CollectionDataProvider<PuppyModel>()
+    /// The cache key for our data provider
+    fileprivate let cacheKey = "Puppies!!!"
     
     lazy var session: URLSession = {
         let configuration = URLSessionConfiguration.ephemeral
@@ -22,21 +26,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // In parallel, we're going to fetch from the cache and fetch from the network
+        // There's no chance of a race condition here, because it's handled by RocketData
+        dataProvider.fetchDataFromCache(withCacheKey: cacheKey) { (_, _) in
+            self.tableView.reloadData()
+        }
         
         session.dataTask(with: URL(string:"https://puppygifs.tumblr.com/api/read/json?callback=csod")!) {
             (data: Data?, response:URLResponse?, error: Error?) in
             if error == nil, let data = data, data.count > 7 {
                 let json = data.subdata(in:5..<data.count-2)
                 
-                let debugStr:NSString = String(data:json, encoding:.utf8)! as NSString
-                print(debugStr.substring(to:14))
-                print(debugStr.substring(from: debugStr.length - 14))
-                
                 let serviceResult = try! JSONDecoder().decode(PuppyService.self, from: json)
-                self.collection = serviceResult.posts.filter({ (model) -> Bool in
+                let collection = serviceResult.posts.filter({ (model) -> Bool in
                     return model.caption?.count ?? 0 > 0 && model.imageURL?.count ?? 0 > 0
                 })
+                
+                self.dataProvider.setData(collection, cacheKey: self.cacheKey)
                 
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
@@ -54,13 +62,13 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.collection.count
+        return self.dataProvider.count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PuppyCell", for: indexPath) as! PuppyCell
         
-        let puppy = collection[indexPath.row]
+        let puppy = dataProvider[indexPath.row]
         
         cell.caption.attributedText = puppy.caption?.html2AttributedString
         
@@ -71,10 +79,18 @@ extension ViewController: UITableViewDataSource {
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let url = collection[indexPath.row].imageURL else { return }
+        guard let url = dataProvider[indexPath.row].imageURL else { return }
         let vc = PuppyImageViewController()
         vc.imageURL = URL(string: url)
         self.show(vc, sender:nil)
     }
     
 }
+
+extension ViewController: CollectionDataProviderDelegate {
+    func collectionDataProviderHasUpdatedData<T>(_ dataProvider: CollectionDataProvider<T>, collectionChanges: CollectionChange, context: Any?) {
+        // Optional: Use collectionChanges to do tableview animations
+        self.tableView.reloadData()
+    }
+}
+
